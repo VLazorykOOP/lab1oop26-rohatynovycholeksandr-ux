bc6708d2646723834e6e096a5a1872964d5a5815
Oleksandr <rohatynovych.oleksandr@chnu.edu.ua>
2026-03-04 00:11:23 +0200

    initial commit

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -2,278 +2,284 @@
 //
 
 #include <iostream>
-#include <fstream>
-#include <ios>
 #include <vector>
-
-#include <time.h>
+#include <fstream>
+#include <ctime>
+#include <cmath>
+#include <windows.h>
 
 using namespace std;
 
-typedef double* pDouble;
-/*
-*   ConsoleInputArrayDouble
-*   
-*/
-int ConsoleInputSizeArray(const int sizeMax)
+//////////////////////////////////////////////////////////////
+// Enable console encoding
+//////////////////////////////////////////////////////////////
+void initConsole()
 {
-    int size = 0; 
-    do {
-        cout << " Input size Array ( 0< 1 < " << sizeMax << " ) ";
-        cin >> size;
-    } while (size <= 0 || size >= sizeMax);
-    return size;
-}
-/*
-*   ConsoleInputArrayDouble
-*
-*/
-int ConsoleInputArray(int sizeMax, double A[])
-{
-    int size = ConsoleInputSizeArray(sizeMax);
-        for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> A[i];
-    }
-    return size;
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
 }
 
-/*
-*   RndInputArrayDouble
-*
-*/
-int RndInputArray(int sizeMax, double A[])
+//////////////////////////////////////////////////////////////
+// 3.1 Input array and save to TEXT file
+//////////////////////////////////////////////////////////////
+void inputArrayText(vector<int>& arr)
 {
-    int size = ConsoleInputSizeArray(sizeMax);
-    int r1=0, r2=0;
-    srand(size);
-
-    for (int i = 0; i < size; i++) {
-        r1 = rand();
-        r2 = rand();
-        A[i] = 100.0 * r1;
-        A[i] = A[i] / (1.0 + r2);
-        cout << A[i] << "   ";
-    }
-    return size;
-}
-
-int ConsoleInputDynamicArrayNew(int sizeMax, pDouble &pA)
-{
-    int size = ConsoleInputSizeArray(sizeMax);
-    pA = new double[size];
-    if (pA == nullptr) { return 0; }
-    for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> pA[i];
-    }
-    return size;
-}
-
-int ConsoleInputDynamicArray_calloc(int sizeMax, pDouble& pA)
-{
-    int size = ConsoleInputSizeArray(sizeMax);
-    pA = (double*)calloc(size, sizeof(double));      // pA = (double*)malloc(size * sizeof(double)); 
-    if (pA == nullptr) { return 0; }
-    for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> pA[i];
-    }
-    return size;
-}
-
-void ConsoleInputVector(int sizeMax, vector<double> &A)
-{
-    int size = ConsoleInputSizeArray(sizeMax);
-    double d;
-    for (int i = 0; i < size; i++) {
-        cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
-    }
-    return ;
-}
-
-
-/*
-*  WriteArrayTextFile 
-*
-*/
-
-void WriteArrayTextFile(int n, double *arr, const char *fileName )
-{
-    ofstream fout(fileName);
-    if (fout.fail()) return;
-    fout << n << endl;
-    for (int i = 0; i < n; i++)
-        fout << arr[i] << "   ";
-    fout.close(); //
-}
-/*
-*  ReadArrayTextFile
-*
-*/
-
-
-int ReadArrayTextFile(int n, double* arr, const char* fileName)
-{
-    int size;
-    ifstream fin(fileName);
-    if (fin.fail()) return 0;
-    fin >> size;
-    if (size <= 0) return 0;
-    if (size > n) size = n;   
-    for (int i = 0; i < n; i++)
-       fin>> arr[i];
-    fin.close();
-    return size;
-    
-}
-
-
-void WriteArrayBinFile(int n, double* arr, const char* fileName)
-{
-    //ios_base
-    ofstream bfout(fileName, ios_base::binary);
-    if (bfout.fail()) return;
-    bfout.write((const char*)&n, sizeof(int));
-    std::streamsize  cn = static_cast<std::streamsize>(n) *sizeof(double);
-    bfout.write((const char*)arr, cn);
-    bfout.close();
-}
-
-int ReadArrayBinFile(int n, double* arr, const char* fileName)
-{
-    int size=0;
-    ifstream bfin(fileName, ios_base::binary);
-    if (bfin.fail()) return 0;
-    bfin.read((char*)&size, sizeof(int));
-    if (size <= 0) return 0;
-    if (size > n) size = n;
-    bfin.read((char*)arr, static_cast<std::streamsize>(size) * sizeof(double));
-    bfin.close();
-    // ssdhs
-    return size;
-}
-
-void ShowMainMenu()
-{
-    cout << "    Main Menu  \n";
-    cout << "    1.  Task 1  \n";
-    cout << "    2.  Task 2  \n";
-    cout << "    3.  Task 3  \n";
-  }
-
-void MenuTask()
-{
-    cout << "     Menu Task   \n";
-    cout << "    1.  Local array  \n";
-    cout << "    2.  Dynamic array 1 \n";
-    cout << "    3.  Dynamic array 2  new \n"; 
-    cout << "    4.  Dynamic array : vector \n";
-    cout << "    5.  Exit \n";
-}
-
-void MenuInput()
-{
-    cout << "     Menu Input   \n";
-    cout << "    1.  Console all \n";
-    cout << "    2.  Console - size, array - random \n";
-    cout << "    3.  File 1.txt \n";
-    cout << "    4.  bb    \n";
-    cout << "    5.  Exit \n";
-}
-
-
-/*
-* Задано одновимірний масив А розміру 2N. 
-* Побудувати два масиви В і С розміру N, 
-* включивши  у масив В елементи масиву А з парними індексами,
-* а у С - з непарними.
-*****************
-*  A - in 
-*  B, C - out 
-*/
-void  TestVariant(int N, double* A, double* B, double* C) {
-    for (int i = 0; i < N; i++) {
-        B[i] = A[2 * i];
-        C[i] = A[2 * i + 1];
-    }
-}
-/*
-*  Task  Var
-* 
-* 
-*/
-void TaskV()
-{
-    char ch = '5',t;
-    do {
-        system("cls");
-        MenuTask();
-        ch = getchar();
-        t=getchar();
-            switch (ch) {
-             case '1': cout << " 1 "; break;
-             case '2': cout << " 2 "; break;
-            //
-            case '5': return;
-            }
-        cout << " Press any key and enter\n";
-        ch = getchar();
-        } while (ch != 27);
-    
-}
-
-void ArrayLocal()
-{
-    double A[1000], B[500], C[500];
     int n;
-    char ch = '5',t;
-    do {
-        system("cls");
-        MenuTask();
-        ch = getchar();
-        t = getchar();
-        switch (ch) {
-        case '1': cout << " 1 "; break;
-        case '2': cout << " 2 "; break;
-            //
-        case '5': return;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    arr.resize(n);
+    cout << "Enter elements:\n";
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    ofstream file("array.txt");
+    for (int x : arr)
+        file << x << " ";
+
+    file.close();
+}
+
+//////////////////////////////////////////////////////////////
+// 3.2 Generate random array + input T + save files
+//////////////////////////////////////////////////////////////
+void randomArrayBinary()
+{
+    srand(time(0));
+    int n;
+
+    cout << "Enter array size: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+        arr[i] = rand() % 201 - 100;
+
+    // INPUT VALUE T
+    int T;
+    cout << "Enter value T: ";
+    cin >> T;
+
+    // save T
+    ofstream tfile("T.txt");
+    tfile << T;
+    tfile.close();
+
+    // save array
+    ofstream file("array.bin", ios::binary);
+    file.write((char*)&n, sizeof(n));
+    file.write((char*)arr.data(), n * sizeof(int));
+    file.close();
+
+    cout << "Array generated and saved.\n";
+    cout << "Value T saved to file.\n";
+}
+
+//////////////////////////////////////////////////////////////
+// Read array from TEXT file
+//////////////////////////////////////////////////////////////
+vector<int> readTextArray()
+{
+    ifstream file("array.txt");
+    vector<int> arr;
+    int x;
+
+    while (file >> x)
+        arr.push_back(x);
+
+    return arr;
+}
+
+//////////////////////////////////////////////////////////////
+// Read array from BINARY file
+//////////////////////////////////////////////////////////////
+vector<int> readBinaryArray()
+{
+    ifstream file("array.bin", ios::binary);
+    int n;
+    file.read((char*)&n, sizeof(n));
+
+    vector<int> arr(n);
+    file.read((char*)arr.data(), n * sizeof(int));
+
+    return arr;
+}
+
+//////////////////////////////////////////////////////////////
+// Read value T from file
+//////////////////////////////////////////////////////////////
+int readT()
+{
+    ifstream file("T.txt");
+    int T;
+    file >> T;
+    return T;
+}
+
+//////////////////////////////////////////////////////////////
+// Remove negative elements
+//////////////////////////////////////////////////////////////
+void removeNegative(vector<int>& arr)
+{
+    vector<int> result;
+
+    for (int x : arr)
+        if (x >= 0)
+            result.push_back(x);
+
+    arr = result;
+}
+
+//////////////////////////////////////////////////////////////
+// Find first maximum negative before T
+//////////////////////////////////////////////////////////////
+void firstMaxNegativeBeforeT(const vector<int>& arr, int T)
+{
+    int maxNeg = -1000000;
+    int index = -1;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == T)
+            break;
+
+        if (arr[i] < 0 && arr[i] > maxNeg)
+        {
+            maxNeg = arr[i];
+            index = i;
         }
-        cout << " Press any key and enter\n";
-        ch = getchar();
-    } while (ch != 27);
-
-}
-
-
-int main()
-{ 
-    
-    
-    
-    const int MAX_SIZE = 560;
-    std::cout << "Hello World!\n";
-    ShowMainMenu();
-    /*
-    double A[MAX_SIZE], B[MAX_SIZE],C[MAX_SIZE];
-    int n,m;
-    n = RndInputArray(MAX_SIZE, A);
-    WriteArrayTextFile(n, A, "1.txt");
-    m = ReadArrayTextFile(MAX_SIZE, B, "1.txt");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << B[i] << "   ";
-    WriteArrayBinFile(n, A, "1.bin");
-    m = ReadArrayBinFile(MAX_SIZE, C, "1.bin");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << C[i] << "   ";
-    cout << "\n  Vector \n";
-    vector<double> vA;
-    ConsoleInputVector(MAX_SIZE, vA);
-    for (auto v : vA) {
-        cout << v << "   ";
     }
-*/
-    TaskV();
-    return 1;
 
+    if (index == -1)
+        cout << "No negative elements found before T\n";
+    else
+        cout << "Index of first maximum negative: " << index << endl;
 }
 
+//////////////////////////////////////////////////////////////
+// Average distance to school
+//////////////////////////////////////////////////////////////
+void averageDistance()
+{
+    double sx, sy;
+    int n;
+
+    cout << "\nEnter school coordinates (x y): ";
+    cin >> sx >> sy;
+
+    cout << "Enter number of houses: ";
+    cin >> n;
+
+    double sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        double x, y;
+        cout << "House " << i + 1 << " (x y): ";
+        cin >> x >> y;
+
+        double dist = sqrt(pow(x - sx, 2) + pow(y - sy, 2));
+        sum += dist;
+    }
+
+    cout << "Average distance: " << sum / n << endl;
+}
+
+//////////////////////////////////////////////////////////////
+// Solve tasks and save result
+//////////////////////////////////////////////////////////////
+void solveTasks()
+{
+    vector<int> arr = readTextArray();
+
+    cout << "\nArray from file:\n";
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+
+    removeNegative(arr);
+
+    ofstream out("result.txt");
+    out << "Array without negative values:\n";
+
+    for (int x : arr)
+    {
+        cout << x << " ";
+        out << x << " ";
+    }
+
+    cout << endl;
+    out.close();
+}
+
+//////////////////////////////////////////////////////////////
+// Read binary file to vector
+//////////////////////////////////////////////////////////////
+void readToVector()
+{
+    vector<int> arr = readBinaryArray();
+
+    cout << "\nData from binary file:\n";
+    for (int x : arr)
+        cout << x << " ";
+
+    cout << endl;
+}
+
+//////////////////////////////////////////////////////////////
+// MAIN MENU
+//////////////////////////////////////////////////////////////
+int main()
+{
+    initConsole();
+
+    vector<int> arr;
+    int choice;
+
+    do
+    {
+        cout << "\n========== MENU ==========\n";
+        cout << "1 - Input array (text file)\n";
+        cout << "2 - Generate array + input T\n";
+        cout << "3 - Remove negatives (from file)\n";
+        cout << "4 - Find max negative before T\n";
+        cout << "5 - Average distance to school\n";
+        cout << "6 - Read binary to vector\n";
+        cout << "0 - Exit\n";
+        cout << "Your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            inputArrayText(arr);
+            break;
+
+        case 2:
+            randomArrayBinary();
+            break;
+
+        case 3:
+            solveTasks();
+            break;
+
+        case 4:
+        {
+            vector<int> arr = readTextArray();
+            int T = readT();
+            firstMaxNegativeBeforeT(arr, T);
+            break;
+        }
+
+        case 5:
+            averageDistance();
+            break;
+
+        case 6:
+            readToVector();
+            break;
+        }
+
+    } while (choice != 0);
+}
